6864f5f4c7fdfc62adb82f8d6ac147f19d27698e
Michael P <30189184+MichaelPokotskyi@users.noreply.github.com>
2023-03-28 10:38:21 -0700

    upd

--- C2A1E1_Macros.h
+++ C2A1E1_Macros.h
@@ -1,6 +1,6 @@
 #ifndef C2A1E1_MACROS_H
 #define C2A1E1_MACROS_H
-#define Product(A, B) (return (a + b))
-#define Negate(A) (return (-A))
-#define Elements(A)
+#define Product(A, B) ((A) * (B))
+#define Negate(A) ((-A))
+#define Elements(A) (sizeof(A))
 #endif // C2A1E1_MACROS_H
