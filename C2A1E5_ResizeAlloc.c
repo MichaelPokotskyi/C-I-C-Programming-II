62e65303e365e67d115ad998078a7257650cf204
Michael P <30189184+MichaelPokotskyi@users.noreply.github.com>
2023-03-30 08:17:28 -0700

    uu

--- C2A1E5_ResizeAlloc.c
+++ C2A1E5_ResizeAlloc.c
@@ -1,5 +1,18 @@
+//
+// Michael Pokotskyi U09662201
+// pokotskyi.m@gmail.com
+// C I C++ Programming II
+// C1 170379 Raymond L. Mitchell
+// 3/28/2023
+// C2A1E5_ResizeAlloc.c
+// Win10
+// Visual C++ 2022, ISO C17
+//
+// Assignment #1 C2A1E5 (C)
+//
 
-
+#include <stdio.h>
 
 void* ResizeAlloc(void* pOld, size_t newSize, size_t oldSize) {
+
 }
