e9f5dcb73d6090f61f60db8db49fa67a3c89c3d9
MiShKa <sly.booking@gmail.com>
2023-03-30 21:49:01 -0700

    uuu

--- C2A1E5_ResizeAlloc.c
+++ C2A1E5_ResizeAlloc.c
@@ -9,10 +9,36 @@
 // Visual C++ 2022, ISO C17
 //
 // Assignment #1 C2A1E5 (C)
-//
+// 
 
 #include <stdio.h>
+#include <malloc.h>
+#include <string.h>
 
 void* ResizeAlloc(void* pOld, size_t newSize, size_t oldSize) {
-
+    //Declare pointer to new block
+    void* new;
+    if (newSize == 0) {
+        return NULL;
+    }
+    else {
+        if ((new = malloc(newSize)) == NULL) {
+            return NULL;
+        }
+        else{
+            if (pOld == NULL) {
+                return new;
+            }
+            else {
+                if (newSize > oldSize) {
+                    memcpy(new, pOld, oldSize);
+                }
+                else {
+                    memcpy(new, pOld, newSize);
+                }
+            }
+        }
+    }
+    free(pOld);
+    return new;
 }
