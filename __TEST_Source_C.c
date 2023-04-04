58180a6956f6cb3a62f44e2c4559e3db78ed9c6e
Michael P <30189184+MichaelPokotskyi@users.noreply.github.com>
2023-04-04 11:11:11 -0700

    uuu

---
+++ __TEST_Source_C.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+#define sum(x, y) x + y
+
+int* fx(void)
+{
+    int x = 5;
+    return(&x);
+}
+
+int main() {
+    //printf("%p", (void*)*fx());
+    printf("%d", 10 * sum(4 + 3, 2));
+    return 0;
+}
