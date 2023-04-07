fa515c7b75052461f205bdc661a19fb30228a082
MiShKa <sly.booking@gmail.com>
2023-04-06 19:46:11 -0700

    uu

--- C2A2E1_CountBitsM.h
+++ C2A2E1_CountBitsM.h
@@ -1,22 +1,22 @@
+////
+//// Michael Pokotskyi U09662201
+//// pokotskyi.m@gmail.com
+//// C I C++ Programming II
+//// C1 170379 Raymond L. Mitchell
+//// 4/5/2023
+//// C2A2E1_CountBitsM.h
+//// Win10
+//// Visual C++ 2022, ISO C17
+////
+//// Assignment #2 C2A2E1 (C)
+//// Determines the number of bits of storage used for the data type 
+//// of objectOrType on any machine on which it is run.
 //
-// Michael Pokotskyi U09662201
-// pokotskyi.m@gmail.com
-// C I C++ Programming II
-// C1 170379 Raymond L. Mitchell
-// 4/5/2023
-// C2A2E1_CountBitsM.h
-// Win10
-// Visual C++ 2022, ISO C17
+//// Include guard to avoid multiple includes
+//#ifndef C2A2E1_COUNTBITSM_H
+//#define C2A2E1_COUNTBITSM_H
 //
-// Assignment #2 C2A2E1 (C)
-// Determines the number of bits of storage used for the data type 
-// of objectOrType on any machine on which it is run.
-
-// Include guard to avoid multiple includes
-#ifndef C2A2E1_COUNTBITSM_H
-#define C2A2E1_COUNTBITSM_H
-
-#include <limits.h>
-#define CountBitsM(objectOrType) (((int)sizeof(objectOrType)) * CHAR_BIT)
-
-#endif // C2A2E1_COUNTBITSM_H
+//#include <limits.h>
+//#define CountBitsM(objectOrType) (((int)sizeof(objectOrType)) * CHAR_BIT)
+//
+//#endif // C2A2E1_COUNTBITSM_H
--- C2A2E1_CountIntBitsF.c
+++ C2A2E1_CountIntBitsF.c
@@ -1,28 +1,28 @@
+////
+//// Michael Pokotskyi U09662201
+//// pokotskyi.m@gmail.com
+//// C I C++ Programming II
+//// C1 170379 Raymond L. Mitchell
+//// 4/5/2023
+//// C2A2E1_CountIntBitsF.c
+//// Win10
+//// Visual C++ 2022, ISO C17
+////
+//// Assignment #2 C2A2E1 (C)
+//// Determines the number of bits used to represent a type int value 
+//// on any machine on which it is run.
 //
-// Michael Pokotskyi U09662201
-// pokotskyi.m@gmail.com
-// C I C++ Programming II
-// C1 170379 Raymond L. Mitchell
-// 4/5/2023
-// C2A2E1_CountIntBitsF.c
-// Win10
-// Visual C++ 2022, ISO C17
+//// The value produced on the same implementation from two 
+//// functions from current assigning cannot be different, since 
+//// any int value will be stored at exactly same bytes size.
 //
-// Assignment #2 C2A2E1 (C)
-// Determines the number of bits used to represent a type int value 
-// on any machine on which it is run.
-
-// The value produced on the same implementation from two 
-// functions from current assigning cannot be different, since 
-// any int value will be stored at exactly same bytes size.
-
-int CountIntBitsF(void) {
-    int count = 0;
-    // loop controlling bits leftshifting
-    for (unsigned int bits_num = 1; bits_num != 0; bits_num <<= 1)
-    {
-        // updating counter of bits
-        ++count;
-    }
-    return count;
-}
+//int CountIntBitsF(void) {
+//    int count = 0;
+//    // loop controlling bits leftshifting
+//    for (unsigned int bits_num = 1; bits_num != 0; bits_num <<= 1)
+//    {
+//        // updating counter of bits
+//        ++count;
+//    }
+//    return count;
+//}
