8bcddc04d57dc597cf8177dcdbdf5b2d1727a75e
MiShKa <sly.booking@gmail.com>
2023-04-11 00:49:10 -0700

    uu

--- C2A2E4_OpenFile.cpp
+++ C2A2E4_OpenFile.cpp
@@ -1,1 +1,25 @@
-1
+//
+// Michael Pokotskyi U09662201
+// pokotskyi.m@gmail.com
+// C I C++ Programming II
+// C1 170379 Raymond L. Mitchell
+// 4/5/2023
+// C2A2E6_OpenFile.cpp
+// Win10
+// Visual C++ 2022, ISO C++ 17
+//
+// Assignment #2 C2A2E4 (C)
+//
+
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+void OpenFile(const char *fileName, ifstream &inFile) {
+    inFile.open(fileName, ios_base::in);
+        if (!inFile.is_open())
+        {
+            cerr << "Open failed: " << fileName << "\n";
+            exit(EXIT_FAILURE);
+        }
+}
--- C2A2E4_Reverse.cpp
+++ C2A2E4_Reverse.cpp
@@ -1,1 +1,42 @@
-1
+//
+// Michael Pokotskyi U09662201
+// pokotskyi.m@gmail.com
+// C I C++ Programming II
+// C1 170379 Raymond L. Mitchell
+// 4/5/2023
+// C2A2E6_Reverse.cpp
+// Win10
+// Visual C++ 2022, ISO C++ 17
+//
+// Assignment #2 C2A2E4 (C)
+// 
+
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+bool isSep(char ch) {
+    if ( isspace(ch) == 0 || ch == '.' || ch == '?' || ch == '!' || ch == ',' ||ch == ':' || ch == ';' || ch ==  EOF)
+    {
+        return false;
+    }
+    else 
+    {
+        return true;
+    }
+ }
+
+int Reverse(ifstream &inFile, const int level) {
+    int thisChar = inFile.get();
+    if (isSep(thisChar))
+    {
+        return thisChar;
+    }
+    else 
+    {
+        int thisSeparator = Reverse(inFile, level + 1);
+        if () {}
+        else {}
+     
+    }
+}
