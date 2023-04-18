c99274647fa6bc6a9caf86848b1a9f8ec07f5159
MiShKa <sly.booking@gmail.com>
2023-04-17 18:10:21 -0700

    uu

--- C2A3E3_RecordOpinions.c
+++ C2A3E3_RecordOpinions.c
@@ -9,33 +9,35 @@
 // Visual C++ 2022, ISO C17
 // Assignment #3 C2A3E3 (C)
 // Function implement a survey of how shoppers like a product by
-// prompting them to enter a decimal integer rating value within an
-// allowed range of BEST to WORST.
+// prompting them to enter a value within an allowed range 
+// of BEST to WORST.
 
-#include<stdio.h>
+#include <stdio.h>
 
 #define BEST 2
 #define WORST (-BEST)
 #define CHOICES (BEST + (BEST) + 1)
 #define TERMINATE 999
+#define CENTER 2
 
 void RecordOpinions(void) {
     int rating[CHOICES] = {0};
-    int input, *ptr = &rating[0];
+    int *ptr = &rating[CHOICES / CENTER];
     
     printf("Allowed rating range are from %d to %d\n"
         "Terminate value are %d\n", BEST, WORST, TERMINATE);
     for (;;)
     {
+        int user_input;
         printf("Enter your rating ");
-        scanf_s("%d", &input);
-        if (input == TERMINATE)
+        scanf_s("%d", &user_input);
+        if (user_input == TERMINATE)
         {
             // designated terminator breaks the loop
             printf("Input terminated.\n");
             break;
         }
-        if (input < WORST || input > BEST)
+        if (user_input < WORST || user_input > BEST)
         {
             // out of range notification and re-enter value
             printf("Out of range rating, input again\n");
@@ -43,11 +45,12 @@ void RecordOpinions(void) {
         else
         {
             // only in-range values are updated, supporting negative indices
-            ptr[input] = input;
+            ptr[user_input]++;
         }
     }
     // formatted output section
-    printf("\nRating    Quantity\n------    --------\n");
+    printf("\nRating    Quantity"
+        "\n------    --------\n");
     for (int output_it = CHOICES - 1; output_it >= 0; output_it--)
     {
         printf("%6d%12d\n", output_it + WORST, rating[output_it]);
