2a9571ca9227098a4a26263dc3f57a705346bffb
MiShKa <sly.booking@gmail.com>
2023-04-18 20:18:45 -0700

    uuu

--- C2A4E1_WorkerFunction.cpp
+++ C2A4E1_WorkerFunction.cpp
@@ -1,1 +1,11 @@
-1
+//
+// Michael Pokotskyi U09662201
+// pokotskyi.m@gmail.com
+// C I C++ Programming II
+// C1 170379 Raymond L. Mitchell
+// 4/18/2023
+// C2A2E1_WorkerFunction.cpp
+// Win10
+// Visual C++ 2022, ISO C17
+// 
+// Assignment #4 C2A4E1 (C)
