2ebc4709088e927e27e8f33a2c5e33b8b1c99955
MiShKa <sly.booking@gmail.com>
2023-04-20 20:53:19 -0700

    cc

--- C2A4E2_StorageMap5D.h
+++ C2A4E2_StorageMap5D.h
@@ -1,28 +1,28 @@
+////
+//// Michael Pokotskyi U09662201
+//// pokotskyi.m@gmail.com
+//// C I C++ Programming II
+//// C1 170379 Raymond L. Mitchell
+//// 4/18/2023
+//// C2A4E2_StorageMap5D.h
+//// Win10
+//// Visual C++ 2022, ISO C17
+//// 
+//// Assignment #4 C2A4E2 (C)
+//// Storage map equation for a 5-dimensional array of arbitrary type having 
+//// arbitrary dimension values.
 //
-// Michael Pokotskyi U09662201
-// pokotskyi.m@gmail.com
-// C I C++ Programming II
-// C1 170379 Raymond L. Mitchell
-// 4/18/2023
-// C2A4E2_StorageMap5D.h
-// Win10
-// Visual C++ 2022, ISO C17
-// 
-// Assignment #4 C2A4E2 (C)
-// Storage map equation for a 5-dimensional array of arbitrary type having 
-// arbitrary dimension values.
-
-// Include guard to avoid multiple includes
-#ifndef C2A4E2_STORAGEMAP5D_H
-#define C2A4E2_STORAGEMAP5D_H
-
-#define StorageMap5D(ptr,\
-idx0, idx1, idx2, idx3, idx4, dim1, dim2, dim3, dim4) \
-(*((ptr) + \
-(idx0) * (dim1) * (dim2) * (dim3) * (dim4) + \
-(idx1) * (dim2) * (dim3) * (dim4) + \
-(idx2) * (dim3) * (dim4) + \
-(idx3) * (dim4) + \
-(idx4) ))
-
-#endif // C2A4E2_STORAGEMAP5D_H
+//// Include guard to avoid multiple includes
+//#ifndef C2A4E2_STORAGEMAP5D_H
+//#define C2A4E2_STORAGEMAP5D_H
+//
+//#define StorageMap5D(ptr,\
+//idx0, idx1, idx2, idx3, idx4, dim1, dim2, dim3, dim4) \
+//(*((ptr) + \
+//(idx0) * (dim1) * (dim2) * (dim3) * (dim4) + \
+//(idx1) * (dim2) * (dim3) * (dim4) + \
+//(idx2) * (dim3) * (dim4) + \
+//(idx3) * (dim4) + \
+//(idx4) ))
+//
+//#endif // C2A4E2_STORAGEMAP5D_H
