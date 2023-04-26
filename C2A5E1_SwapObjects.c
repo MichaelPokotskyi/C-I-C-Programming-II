42d8bd5e399114f8af227d98b89665839af670b1
MiShKa <sly.booking@gmail.com>
2023-04-25 21:15:16 -0700

    uu

--- C2A5E1_SwapObjects.c
+++ C2A5E1_SwapObjects.c
@@ -9,21 +9,26 @@
 // Visual C++ 2022, ISO C17
 //
 // Assignment #3 C2A5E1 (C)
-// 
+// SwapObjects dynamically allocates memory size and swapping two pointers
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
-void SwapObjects(void* pa, void* pb, size_t size) {
-    void* temp;
+void SwapObjects(void *pa, void *pb, size_t size) {
+    void *temp;
+    // memory allocation and testing allocation fail
     if ((temp = malloc(size)) == NULL)
     {
         fprintf(stderr, "Memory allocation error\n");
         exit(EXIT_FAILURE);
     }
+    // pa to temp
     memcpy(temp, pa, size);
+    // pa to pb
     memcpy(pa, pb, size);
+    // temp to pb
     memcpy(pb, temp, size);
-    free (temp);
+    // free temp
+    free(temp);
 }
