836e8e39bd6cf84655724166b1699c6581a86051
MiShKa <sly.booking@gmail.com>
2023-04-25 23:22:37 -0700

    uu

--- C2A5E2_Create2D.c
+++ C2A5E2_Create2D.c
@@ -4,7 +4,7 @@
 // C I C++ Programming II
 // C1 170379 Raymond L. Mitchell
 // 4/25/2023
-// C2A5E2_SwapObjects.c
+// C2A5E2_Create2D.c
 // Win10
 // Visual C++ 2022, ISO C17
 //
@@ -12,10 +12,32 @@
 //
 
 #include <stdio.h>
+#include <stdlib.h>
+//#include <string.h>
 #include "C2A5E2_Type-Driver.h"
 
 Type** Create2D(size_t rows, size_t cols) {
+    Type* p0, ** p1, **p2, ** end;
+    if ((p0 = malloc(rows * sizeof(Type*))) == NULL)
+    {
+        fprintf(stderr, "Memory allocation failed\n");
+        exit(EXIT_FAILURE);
+    }
+    p1 = (Type**)p0;
+    cols *= sizeof(Type);
+    p0 = NULL;
+    if ((p0 = malloc(cols)) == NULL)
+    {
+        fprintf(stderr, "Memory allocation failed\n");
+        exit(EXIT_FAILURE);
+    }
+    for (end = p1 + rows, p2 = p1; p2 < end; ++p2)
+    {
+        *p2 = (Type*)p0;
+    }
+    return p1;
 }
 
 void Free2D(void* p) {
+    
 }
