5c6e3d2a15383ae005de65cdb9665daa5dd0bd65
MiShKa <sly.booking@gmail.com>
2023-04-26 19:21:32 -0700

    hhh

--- C2A5E2_Create2D.c
+++ C2A5E2_Create2D.c
@@ -9,54 +9,35 @@
 // Visual C++ 2022, ISO C17
 //
 // Assignment #5 C2A5E2 (C)
-//
+// Creates a 2-dimensional pointer array of type Type having the number of rows
+// and columns. All memory needed for this array is dynamically allocated at 
+// once using a single call to malloc.
 
 #include <stdio.h>
 #include <stdlib.h>
-//#include <string.h>
 #include "C2A5E2_Type-Driver.h"
 
-Type** Create2D(size_t rows, size_t cols) {
-
-    Type *p0, **p1, **p2, **end;
-    size_t ttl = (rows * cols) * sizeof(Type);
-    p1 = malloc(ttl * sizeof(Type*));
-
-    for (int i = 0; i < rows; i++)
-    {
-        for (int j = 0; j < cols; j++) 
-        {
-            p1 = p2
-        }
-    }
-
-    // p1 = (Type**)p0;
-    //for (end = p1 + ) {}
-
-
-
-    /*Type *p0, **p1, **p2, **end;
-    if ((p0 = malloc(rows * sizeof(Type*))) == NULL)
+Type **Create2D(size_t rows, size_t cols) {
+    // total size to allocate
+    size_t ttl = rows * sizeof(Type *) + rows * cols * sizeof(Type);
+    Type *p;
+    // for single allocation
+    Type **pSingle;
+    // memory allocating with failure test
+    if ((pSingle = (Type **)malloc(ttl)) == NULL)
     {
         fprintf(stderr, "Memory allocation failed\n");
         exit(EXIT_FAILURE);
     }
-    p1 = (Type**)p0;
-    cols *= sizeof(Type);
-    p0 = NULL;
-    if ((p0 = malloc(cols)) == NULL)
+    p = (Type *)(pSingle + rows);
+    for (size_t elem = 0; elem < rows; ++elem)
     {
-        fprintf(stderr, "Memory allocation failed\n");
-        exit(EXIT_FAILURE);
+        pSingle[elem] = p + cols * elem;
     }
-    for (end = p1 + rows, p2 = p1; p2 < end; ++p2)
-    {
-        *p2 = (Type*)p0;
-    }
-    return p1;*/
-    return p1;
+    return pSingle;
 }
 
-void Free2D(void* p) {
-    
+void Free2D(void *p) {
+    // free memory allocation
+    free(p);
 }
