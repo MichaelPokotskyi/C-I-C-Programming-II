081d1b2e34425014a6776506b0ae69001a4234a3
MiShKa <sly.booking@gmail.com>
2023-05-04 21:58:25 -0700

    kkk

---
+++ C2A6E1_GetPointers.c
@@ -0,0 +1,23 @@
+//
+// Michael Pokotskyi U09662201
+// pokotskyi.m@gmail.com
+// C I C++ Programming II
+// C1 170379 Raymond L. Mitchell
+// 5/2/2023
+// C2A6E1_GetPointers.c
+// Win10
+// Visual C++ 2022, ISO C17
+//
+// Assignment #1 C2A6E1 (C)
+// Two pointers to standard library functions declared and returned.
+
+#include <stdio.h>
+
+int (*GetPrintfPointer(void))(const char *format, ...) {
+    int (*pPrintf)(const char *, ...) = printf;
+    return pPrintf;
+}
+int (*GetPutsPointer(void))(const char *str) {
+    int (*pPuts)(const char *) = puts;
+    return pPuts;
+}
