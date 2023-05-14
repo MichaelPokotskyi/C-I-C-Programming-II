e273e03d965deb3b347671cb43cc4074806cb227
MiShKa <sly.booking@gmail.com>
2023-05-13 21:06:02 -0700

    yy

--- C2A7E2_ListHex.cpp
+++ C2A7E2_ListHex.cpp
@@ -1,43 +1,47 @@
+////
+//// Michael Pokotskyi U09662201
+//// pokotskyi.m@gmail.com
+//// C I C++ Programming II
+//// C1 170379 Raymond L. Mitchell
+//// 5/10/2023
+//// C2A7E2_ListHex.cpp
+//// Win10
+//// Visual C++ 2022, ISO C++ 17
+////
+//// Assignment #7 C2A7E2 (C)
+//// Displays the contents of the file in inFile as one pair of hexadecimal
+//// characters per file byte, zero - filled on the left if necessary to
+//// produce the two characters.
 //
-// Michael Pokotskyi U09662201
-// pokotskyi.m@gmail.com
-// C I C++ Programming II
-// C1 170379 Raymond L. Mitchell
-// 5/10/2023
-// C2A7E2_ListHex.cpp
-// Win10
-// Visual C++ 2022, ISO C++ 17
+//#include <iostream>
+//#include <fstream>
+//#include <iomanip>
+//using namespace std;
 //
-// Assignment #7 C2A7E2 (C)
-// Displays the contents of the file in inFile as one pair of hexadecimal
-// characters per file byte, zero - filled on the left if necessary to
-// produce the two characters.
-
-#include <iostream>
-#include <fstream>
-#include <iomanip>
-using namespace std;
-
-const int _WIDTH = 2;
-const int _COUNT = 1;
-const int _ZERO = 0;
-
-void ListHex(ifstream& inFile, int bytesPLine){
-    int buf;
-    // set fill and hex output
-    cout << setfill('0') << hex;
-    int count = _COUNT;
-    while ((buf = inFile.get()) != EOF)
-        {
-            if (count % bytesPLine == _ZERO)
-            {
-                cout << setw(_WIDTH) << (unsigned int)buf << '\n';
-            }
-            else
-            {
-                // check here to detect if it a last value...and print no space
-                cout << setw(_WIDTH) << (unsigned int)buf << " ";
-            }
-            count++;
-        }
-} // DRAFT
+//const int WIDTH = 2;
+//const int COUNT = 1;
+//const int NUMZER0 = 0;
+//
+//void ListHex(ifstream& inFile, int bytesPerLine) {
+//    cout << hex << setfill('0');              
+//    int byte, bytesOnThisLine = NUMZER0;
+//    while ((byte = inFile.get()) != EOF)
+//    {
+//        // if not a time for a new line 
+//        if (bytesOnThisLine != NUMZER0) 
+//        {
+//            cout << ' ';
+//        }
+//        cout << setw(WIDTH) << byte;
+//        // if time for new line
+//        if (++bytesOnThisLine == bytesPerLine)
+//        {
+//            // reset counter
+//            bytesOnThisLine = NUMZER0;
+//            cout << '\n';
+//        }
+//    }
+//    // this statement add newline after while loop finished
+//    if (bytesOnThisLine != NUMZER0)
+//        cout << '\n';
+//}
--- C2A7E2_OpenFileBinary.cpp
+++ C2A7E2_OpenFileBinary.cpp
@@ -1,26 +1,26 @@
+////
+//// Michael Pokotskyi U09662201
+//// pokotskyi.m@gmail.com
+//// C I C++ Programming II
+//// C1 170379 Raymond L. Mitchell
+//// 5/10/2023
+//// C2A7E2_OpenFileBinary.cpp
+//// Win10
+//// Visual C++ 2022, ISO C++ 17
+////
+//// Assignment #7 C2A7E2 (C)
+//// Opens the file named in fileName in the binary mode using the inFile object.
 //
-// Michael Pokotskyi U09662201
-// pokotskyi.m@gmail.com
-// C I C++ Programming II
-// C1 170379 Raymond L. Mitchell
-// 5/10/2023
-// C2A7E2_OpenFileBinary.cpp
-// Win10
-// Visual C++ 2022, ISO C++ 17
+//#include <iostream>
+//#include <fstream>
+//using namespace std;
 //
-// Assignment #7 C2A7E2 (C)
-// Opens the file named in fileName in the binary mode using the inFile object.
-
-#include <iostream>
-#include <fstream>
-using namespace std;
-
-void OpenFileBinary(const char *fileName, ifstream &inFile){
-    inFile.open(fileName, ifstream::in | ifstream::binary);
-    // open fail test
-    if (!inFile.is_open())
-    {
-        cerr << "File failed to open " << fileName << "\n";
-        exit(EXIT_FAILURE);
-    }
-}
+//void OpenFileBinary(const char *fileName, ifstream &inFile){
+//    inFile.open(fileName, ifstream::in | ifstream::binary);
+//    // open fail test
+//    if (!inFile.is_open())
+//    {
+//        cerr << "File failed to open " << fileName << "\n";
+//        exit(EXIT_FAILURE);
+//    }
+//}
